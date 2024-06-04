a09b33bd808f8dd182f66a6767747e19571544a9
Joelbu <89338010+Joelbu537@users.noreply.github.com>
2024-06-04 09:33:38 +0200

    remade code

--- Program.cs
+++ Program.cs
@@ -1,104 +1,74 @@
-﻿using System.Net;
-using System.Net.Sockets;
+﻿using System.Net.Sockets;
 using System.Text;
+using System.Net;
+using System;
 
 class Program
 {
-    static string Version = "0.0.3";
-    static async void Main(string[] args)
+    static void Main(string[] args)
     {
-        if(args.Length != 0)
+        try
         {
-            switch(args[0])
+            TcpListener server = new TcpListener(IPAddress.Any, 5000);
+            server.Start();
+            Console.WriteLine("Lise-Meitner Gymnasium Leverkusen");
+            Console.WriteLine("Server starting...");
+
+            while (true)
             {
-                case "help":
-                    break;
-                case "reset":
-                    break;
-                case "addstudent":
-                    break;
+                Console.WriteLine("Server active");
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Connection incomming...");
+                ThreadPool.QueueUserWorkItem(HandleClient, client);
             }
-        }
-        while (true)
+        }catch (Exception ex)
         {
-            try
-            {
-                Console.WriteLine("Lise Meinter Gymnasium Leverkusen Lernzeit-App Server Application " + Version);
-                Console.WriteLine("Starting tcplistener...");
-                TcpListener listener = new TcpListener(IPAddress.Any, 33533);
-                listener.Start();
-                Console.WriteLine("Starting tcpclient");
-                TcpClient client = await listener.AcceptTcpClientAsync();
-                Console.WriteLine("Incoming connection...");
-                try
-                {
-                    Task.Run(() => HandleClient(client));
-                }
-                catch (Exception ex)
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine("Connection could not be established!");
-                    Console.WriteLine(ex.ToString());
-                    Console.ResetColor();
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(ex.ToString());
-                Console.ResetColor();
-                Console.ReadKey();
-            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(ex.Message);
+            Console.ResetColor();
+            Console.ReadKey();
         }
     }
-    static void HandleClient(TcpClient client)
+    static void HandleClient(object obj)
     {
-        using (NetworkStream stream = client.GetStream())
+        TcpClient client = (TcpClient)obj;
+        NetworkStream stream = client.GetStream();
+        Console.WriteLine($"Connection established with {client.Client.RemoteEndPoint}");
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+
+        try
         {
-            Console.WriteLine($"Connected to {client.Client.RemoteEndPoint}!");
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            try
+            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
             {
-                Thread.Sleep(300);
-                bytesRead = stream.Read(buffer, 0, buffer.Length);
-                Console.WriteLine($"Received stream content ({bytesRead})");
-                string datareceived = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-                string[] content = datareceived.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+                string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Nachricht erhalten: " + message);
 
-                string reply = "";
-                byte[] reply_bytes;
-
-                switch (content[0])
-                {
-                    default:
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine($"Unknown request sent by {client.Client.RemoteEndPoint}");
-                        Console.ResetColor();
-                        stream.Close();
-                        client.Close();
-                        break;
-                    case "verify":
-                        int status = Verify(content[1], content[2]);
-                        reply = $"verify\r\n{status}";
-                        reply_bytes = Encoding.UTF8.GetBytes(reply);
-                        stream.Write(reply_bytes, 0, reply_bytes.Length);
-                        Thread.Sleep(300);
-                        break;
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine($"{client.Client.RemoteEndPoint} ran into a critical error");
-                Console.WriteLine(ex.ToString());
-                Console.ResetColor();
+                string response = HandleMessage(message);
+                byte[] responseBytes = Encoding.ASCII.GetBytes(response);
+                stream.Write(responseBytes, 0, responseBytes.Length);
             }
         }
+        catch (Exception e)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Client error: " + e.Message);
+            Console.ResetColor();
+        }
+        finally
+        {
+            client.Close();
+        }
     }
-    static int Verify(string email, string hash)
+    static string HandleMessage(string message)
     {
-        //Datenbankzeug
-        return 1;
+        if (message == "Hello World!")
+        {
+            return "Hello!";
+        }
+        else
+        {
+            return "0";
+        }
     }
 }
